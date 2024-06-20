4e97b9f5bfabb7dad7c14254f4036bc7ca829226
Shin-sibainu <shincode0712@gmail.com>
2024-06-20 10:36:07 +0900

    mod

--- todo-with-hono-frontend/src/test/e2e/todo-app.spec.ts
+++ todo-with-hono-frontend/src/test/e2e/todo-app.spec.ts
@@ -1,3 +1,5 @@
+//https://tech.revcomm.co.jp/e2e-test-practice-with-playwright
+
 import test, { expect } from "@playwright/test";
 
 // 各テストの開始前に todos 配列を初期状態にリセット
@@ -18,5 +20,23 @@ test.describe("Todo Application", () => {
 
     // Todoリストに入力した内容が表示されているか確認
     await expect(page.locator("text=Learn Playwright")).toBeVisible();
+  });
+
+  test("Todoを削除", async ({ page }) => {
+    // 新しいTodoを追加
+    await page.fill('input[placeholder="Add a new task"]', "Delete this task");
+    await page.click("text=Add");
+
+    // 削除ボタンをクリックしてTodoを削除
+    await page.waitForSelector('button[aria-label="削除"]', {
+      state: "visible",
+    });
+    // "Delete this task"というテキストを持つTodo項目の削除ボタンをクリック
+    await page.click(
+      'text="Delete this task" >> ../.. >> button[aria-label="削除"]'
+    );
+
+    // Todoがリストから消えたことを確認
+    await expect(page.locator("text=Delete this task")).not.toBeVisible();
   });
 });
