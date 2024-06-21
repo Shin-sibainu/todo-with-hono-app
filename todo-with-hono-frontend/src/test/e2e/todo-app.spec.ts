e86ecf4b00eb69a08d4a8695d7f7fea3f8a06c6f
Shin-sibainu <shincode0712@gmail.com>
2024-06-21 10:42:08 +0900

    delete task test

--- todo-with-hono-frontend/src/test/e2e/todo-app.spec.ts
+++ todo-with-hono-frontend/src/test/e2e/todo-app.spec.ts
@@ -2,41 +2,29 @@
 
 import test, { expect } from "@playwright/test";
 
-// 各テストの開始前に todos 配列を初期状態にリセット
-const todos = [
-  { id: 1, title: "Test Todo", status: "todo" },
-  { id: 2, title: "散歩", status: "done" },
-];
-
 test.describe("Todo Application", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://todo-fronend.pages.dev/");
+    await page.goto("https://todo-fronend.pages.dev/", {
+      waitUntil: "networkidle",
+    });
   });
 
   test("Todoを追加", async ({ page }) => {
     // 新しいTodoを入力して追加
     await page.fill('input[placeholder="Add a new task"]', "Learn Playwright");
     await page.click("text=Add");
-
     // Todoリストに入力した内容が表示されているか確認
-    await expect(page.locator("text=Learn Playwright")).toBeVisible();
+    await expect(page.locator("text=Learn Playwright").last()).toBeVisible();
   });
 
-  test("Todoを削除", async ({ page }) => {
-    // 新しいTodoを追加
-    await page.fill('input[placeholder="Add a new task"]', "Delete this task");
+  test("Todoを追加して削除", async ({ page }) => {
+    await page.fill('input[placeholder="Add a new task"]', "Delete Task");
     await page.click("text=Add");
 
-    // 削除ボタンをクリックしてTodoを削除
-    await page.waitForSelector('button[aria-label="削除"]', {
-      state: "visible",
-    });
-    // "Delete this task"というテキストを持つTodo項目の削除ボタンをクリック
-    await page.click(
-      'text="Delete this task" >> ../.. >> button[aria-label="削除"]'
-    );
+    const todoListArea = page.locator('[aria-label="todo-list"]');
+    await todoListArea.last().locator("role=button", { hasText: "Delete Task" })
+      .click;
 
-    // Todoがリストから消えたことを確認
-    await expect(page.locator("text=Delete this task")).not.toBeVisible();
+    await expect(page.locator('text="Delete Task"').last()).not.toBeVisible();
   });
 });
